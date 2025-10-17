c29324d1394182e1d1d041237aa97c9537a4bc1d
Brian Gow <briangow@mit.edu>
2025-10-17 15:39:52 -0400

    add 64 bit option to obf_id

--- etl/setup/udf_obfuscation.sql
+++ etl/setup/udf_obfuscation.sql
@@ -1,13 +1,17 @@
 -- -------------------------------------------------------------------
 -- Obfuscation UDF
--- Uses global constants from configuration: @obf_c, @obf_o
--- Deterministic permutation over 32-bit unsigned space:
---   y = (x * C + O) mod 2^32
--- Implemented via masking with 0xFFFFFFFF.
--- NOTE: Input assumed non-negative INT64 fitting in 32 bits.
+-- Uses global constants from configuration: @obf_c, @obf_o, @obf64_c, @obf64_o
+-- Deterministic permutation over 32-bit or 64-bit unsigned space:
+--   32-bit: y = (x * C + O) mod 2^32, implemented via masking with 0xFFFFFFFF
+--   64-bit: y = (x * C + O) mod 2^64
+-- NOTE: Input assumed non-negative INT64. User must specify bit width (32 or 64).
 -- -------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION `@etl_project.@etl_dataset`.obf_id(x INT64)
+CREATE OR REPLACE FUNCTION `@etl_project.@etl_dataset.obf_id`(x INT64, bits INT64)
 RETURNS INT64 AS (
-  ((x * CAST(@obf_c AS INT64) + CAST(@obf_o AS INT64)) & 0xFFFFFFFF)
+  CASE 
+    WHEN bits = 32 THEN ((x * CAST(@obf_c AS INT64) + CAST(@obf_o AS INT64)) & 0xFFFFFFFF)
+    WHEN bits = 64 THEN (x * CAST(@obf64_c AS INT64) + CAST(@obf64_o AS INT64))
+    ELSE ERROR('Unsupported bit width')
+  END
 );
