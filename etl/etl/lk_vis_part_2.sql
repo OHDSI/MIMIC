e7cefd9206e9ae8ca7d12a521d36e731b119c4b4
Brian Gow <briangow@mit.edu>
2025-10-22 12:57:32 -0400

    use hadm_id for visit_source_value

--- etl/etl/lk_vis_part_2.sql
+++ etl/etl/lk_vis_part_2.sql
@@ -185,10 +185,7 @@ SELECT
     src.admission_type                              AS admission_type, -- current location
     src.admission_location                          AS admission_location, -- to hospital
     src.discharge_location                          AS discharge_location, -- from hospital
-    CONCAT(
-        CAST(src.subject_id AS STRING), '|',
-        CAST(src.hadm_id AS STRING)
-    )                                               AS source_value,
+    CAST(src.hadm_id AS STRING)                     AS source_value,
     -- 
     src.unit_id                     AS unit_id,
     src.load_table_id               AS load_table_id,
@@ -207,10 +204,7 @@ SELECT
     src.admission_type                              AS admission_type, -- current location
     src.admission_location                          AS admission_location, -- to hospital
     src.discharge_location                          AS discharge_location, -- from hospital
-    CONCAT(
-        CAST(src.subject_id AS STRING), '|',
-        CAST(src.date_id AS STRING)
-    )                                               AS source_value,
+    CAST(NULL AS STRING)                            AS source_value,
     -- 
     src.unit_id                     AS unit_id,
     src.load_table_id               AS load_table_id,
@@ -235,11 +229,7 @@ SELECT
     src.date_id                                     AS date_id,
     src.start_datetime                              AS start_datetime,
     src.end_datetime                                AS end_datetime,  -- if null, populate with next start_datetime
-    CONCAT(
-        CAST(src.subject_id AS STRING), '|',
-        COALESCE(CAST(src.hadm_id AS STRING), CAST(src.date_id AS STRING)), '|',
-        CAST(src.transfer_id AS STRING)
-    )                                               AS source_value,
+    CAST(src.hadm_id AS STRING)                     AS source_value,
     src.current_location                            AS current_location, -- find prev and next for adm and disch location
     -- 
     src.unit_id                     AS unit_id,
@@ -266,10 +256,7 @@ SELECT
     CAST(src.start_datetime AS DATE)                AS date_id,
     src.start_datetime                              AS start_datetime,
     CAST(NULL AS DATETIME)                          AS end_datetime,  -- if null, populate with next start_datetime
-    CONCAT(
-        CAST(src.subject_id AS STRING), '|',
-        CAST(src.hadm_id AS STRING)
-    )                                               AS source_value,
+    CAST(src.hadm_id AS STRING)                     AS source_value,
     src.admission_type                              AS current_location, -- find prev and next for adm and disch location
     -- 
     src.unit_id                     AS unit_id,
@@ -296,11 +283,7 @@ SELECT
     CAST(src.start_datetime AS DATE)                AS date_id,
     src.start_datetime                              AS start_datetime,
     src.end_datetime                                AS end_datetime,
-    CONCAT(
-        CAST(src.subject_id AS STRING), '|',
-        CAST(src.hadm_id AS STRING), '|',
-        CAST(src.start_datetime AS STRING)
-    )                                               AS source_value,
+    CAST(src.hadm_id AS STRING)                     AS source_value,
     src.curr_service                                AS current_location,
     -- 
     src.unit_id                     AS unit_id,
@@ -327,7 +310,7 @@ SELECT
     src.date_id                                     AS date_id,
     src.start_datetime                              AS start_datetime,
     src.end_datetime                                AS end_datetime,  -- if null, populate with next start_datetime
-    src.reference_id                                AS source_value,
+    CAST(src.hadm_id AS STRING)                     AS source_value,
     src.current_location                            AS current_location, -- find prev and next for adm and disch location
     -- 
     src.unit_id                     AS unit_id,
