5a47a486c7a948f88b53891c3637367df0ee837b
Brian Gow <briangow@mit.edu>
2026-03-04 11:25:12 -0500

    update vocab view creation

--- scripts/create_vocab_views.sql
+++ scripts/create_vocab_views.sql
@@ -8,36 +8,44 @@
 -- ============================================================================
 
 -- Create dataset if not exists
-CREATE SCHEMA IF NOT EXISTS `lcp-internal.mimiciv_chorus_year3_vocab_views`
-  OPTIONS(
-    description="Unified vocabulary views combining Athena master and custom vocabularies"
-  );
+-- CREATE SCHEMA IF NOT EXISTS `lcp-internal.mimiciv_chorus_year3_vocab_views`
+--   OPTIONS(
+--     description="Unified vocabulary views combining Athena master and custom vocabularies"
+--   );
 
 -- ============================================================================
 -- CONCEPT table
 -- Includes all Athena concepts + custom concepts not in Athena
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.concept` AS
+-- When Athena or custom vocab is updated, run this to update the view:
+CALL BQ.REFRESH_MATERIALIZED_VIEW('lcp-internal.mimiciv_chorus_year3_cdm.voc_concept');
+
+
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.concept`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept`
-WHERE concept_id NOT IN (
-  SELECT concept_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept`
-)
+-- WHERE concept_id NOT IN (
+--   SELECT concept_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept`
+-- )
 UNION ALL
-SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.concept`
-WHERE concept_id NOT IN (
-  SELECT concept_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept`
-)
-  AND concept_id NOT IN (
-  SELECT concept_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept`
-);
+SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.concept`;
+-- WHERE concept_id NOT IN (
+--   SELECT concept_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept`
+-- )
+--   AND concept_id NOT IN (
+--   SELECT concept_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept`
+-- );
 
 -- ============================================================================
 -- CONCEPT_RELATIONSHIP table
 -- Includes all Athena relationships + custom relationships
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.concept_relationship` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_relationship`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_relationship` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_relationship`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept_relationship`
@@ -48,7 +56,9 @@ SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.concept_relationsh
 -- CONCEPT_ANCESTOR table
 -- Includes all Athena ancestor relationships + custom
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.concept_ancestor` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_ancestor`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_ancestor` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_ancestor`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept_ancestor`
@@ -59,7 +69,9 @@ SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.concept_ancestor`;
 -- CONCEPT_SYNONYM table
 -- Includes all Athena synonyms + custom
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.concept_synonym` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_synonym`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_synonym` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_synonym`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept_synonym`
@@ -70,7 +82,9 @@ SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.concept_synonym`;
 -- DRUG_STRENGTH table
 -- Includes all Athena drug strengths + custom
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.drug_strength` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_drug_strength`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_drug_strength` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.drug_strength`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.drug_strength`
@@ -82,86 +96,92 @@ SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.drug_strength`;
 -- Metadata about vocabularies themselves
 -- Union all; custom vocabs may define new vocabulary IDs
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.vocabulary` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_vocabulary`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_vocabulary` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.vocabulary`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.vocabulary`
-WHERE vocabulary_id NOT IN (
-  SELECT vocabulary_id FROM `lcp-internal.mimiciv_chorus_year3_athena.vocabulary`
-)
+-- WHERE vocabulary_id NOT IN (
+--   SELECT vocabulary_id FROM `lcp-internal.mimiciv_chorus_year3_athena.vocabulary`
+-- )
 UNION ALL
-SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.vocabulary`
-WHERE vocabulary_id NOT IN (
-  SELECT vocabulary_id FROM `lcp-internal.mimiciv_chorus_year3_athena.vocabulary`
-)
-  AND vocabulary_id NOT IN (
-  SELECT vocabulary_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.vocabulary`
-);
+SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.vocabulary`;
+-- WHERE vocabulary_id NOT IN (
+--   SELECT vocabulary_id FROM `lcp-internal.mimiciv_chorus_year3_athena.vocabulary`
+-- )
+--   AND vocabulary_id NOT IN (
+--   SELECT vocabulary_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.vocabulary`
+-- );
 
 -- ============================================================================
 -- CONCEPT_CLASS table
 -- Standard OMOP reference table; usually no custom entries
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.concept_class` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_class`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_concept_class` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_class`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept_class`
-WHERE concept_class_id NOT IN (
-  SELECT concept_class_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_class`
-)
+-- WHERE concept_class_id NOT IN (
+--   SELECT concept_class_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_class`
+-- )
 UNION ALL
-SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.concept_class`
-WHERE concept_class_id NOT IN (
-  SELECT concept_class_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_class`
-)
-  AND concept_class_id NOT IN (
-  SELECT concept_class_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept_class`
-);
+SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.concept_class`;
+-- WHERE concept_class_id NOT IN (
+--   SELECT concept_class_id FROM `lcp-internal.mimiciv_chorus_year3_athena.concept_class`
+-- )
+--   AND concept_class_id NOT IN (
+--   SELECT concept_class_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.concept_class`
+-- );
 
 -- ============================================================================
 -- DOMAIN table
 -- Standard OMOP reference table; usually no custom entries
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.domain` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_domain`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_domain` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.domain`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.domain`
-WHERE domain_id NOT IN (
-  SELECT domain_id FROM `lcp-internal.mimiciv_chorus_year3_athena.domain`
-)
+-- WHERE domain_id NOT IN (
+--   SELECT domain_id FROM `lcp-internal.mimiciv_chorus_year3_athena.domain`
+-- )
 UNION ALL
-SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.domain`
-WHERE domain_id NOT IN (
-  SELECT domain_id FROM `lcp-internal.mimiciv_chorus_year3_athena.domain`
-)
-  AND domain_id NOT IN (
-  SELECT domain_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.domain`
-);
+SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.domain`;
+-- WHERE domain_id NOT IN (
+--   SELECT domain_id FROM `lcp-internal.mimiciv_chorus_year3_athena.domain`
+-- )
+--   AND domain_id NOT IN (
+--   SELECT domain_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.domain`
+-- );
 
 -- ============================================================================
 -- RELATIONSHIP table
 -- Standard OMOP reference table; maps relationship types
 -- ============================================================================
-CREATE OR REPLACE VIEW `lcp-internal.mimiciv_chorus_year3_vocab_views.relationship` AS
+DROP MATERIALIZED VIEW IF EXISTS `lcp-internal.mimiciv_chorus_year3_cdm.voc_relationship`;
+
+CREATE MATERIALIZED VIEW `lcp-internal.mimiciv_chorus_year3_cdm.voc_relationship` AS
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_athena.relationship`
 UNION ALL
 SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.relationship`
-WHERE relationship_id NOT IN (
-  SELECT relationship_id FROM `lcp-internal.mimiciv_chorus_year3_athena.relationship`
-)
+-- WHERE relationship_id NOT IN (
+--   SELECT relationship_id FROM `lcp-internal.mimiciv_chorus_year3_athena.relationship`
+-- )
 UNION ALL
-SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.relationship`
-WHERE relationship_id NOT IN (
-  SELECT relationship_id FROM `lcp-internal.mimiciv_chorus_year3_athena.relationship`
-)
-  AND relationship_id NOT IN (
-  SELECT relationship_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.relationship`
-);
+SELECT * FROM `lcp-internal.mimiciv_chorus_year3_cvb_waveform.relationship`;
+-- WHERE relationship_id NOT IN (
+--   SELECT relationship_id FROM `lcp-internal.mimiciv_chorus_year3_athena.relationship`
+-- )
+--   AND relationship_id NOT IN (
+--   SELECT relationship_id FROM `lcp-internal.mimiciv_chorus_year3_cvb_mimic.relationship`
+-- );
 
 -- ============================================================================
 -- Summary
 -- ============================================================================
--- All views created successfully in lcp-internal.mimiciv_chorus_year3_vocab_views
+-- All views created successfully in lcp-internal.mimiciv_chorus_year3_cdm
 -- Next step: Update your ETL config to point to this dataset
--- Example in full.etlconf:
---   "@voc_dataset": "mimiciv_chorus_year3_vocab_views"
